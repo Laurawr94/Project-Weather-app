ca203146c8a5198ff196a4bb73707dec289226c8
Laurawr94 <107041167+Laurawr94@users.noreply.github.com>
2022-08-27 18:02:19 +1000

    updated JS date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function currentTemp(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -9,6 +32,9 @@ function currentTemp(response) {
     response.data.main.humidity;
   document.querySelector(".currentWind").innerHTML = Math.round(
     response.data.wind.speed
+  );
+  document.querySelector(".dateTime").innerHTML = formatDate(
+    response.data.dt * 1000
   );
 }
 
